bf8bb3cc2950df01da52866fa30bc3a780014c57
Olimjon <a.abdualiym@gmail.com>
2020-08-16 18:40:41 +0500

    fix url manager

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -2,6 +2,13 @@
 
 use codemix\localeurls\UrlManager;
 
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
 return [
     'class' => UrlManager::class,
     'hostInfo' => $params['siteUrl'],
@@ -25,4 +32,5 @@ return [
         '<_c:[\w\-]+>' => '<_c>/index',
         '<_c:[\w\-]+>/<id:[\d+]+>' => '<_c>/view',
         '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
-    ],];
+    ],
+];
